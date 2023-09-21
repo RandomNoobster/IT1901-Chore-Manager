c4602306a82ba58f893a8b3179ca386046cbde24
Kristoffer <krisose@stud.ntnu.no>
2023-09-21 13:15:53 +0200

    Implementing tests for Day class

---
+++ chore-manager/core/src/test/java/core/Data/DayTest.java
@@ -0,0 +1,34 @@
+package core.Data;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DayTest {
+    Day day;
+    LocalDate date;
+
+    @BeforeEach
+    public void populateDay() {
+        this.date = LocalDate.of(2020, 1, 1);
+        this.day = new Day(this.date);
+    }
+
+    @Test
+    public void testGetDate() {
+        assertDoesNotThrow(() -> this.day.getDate());
+        assertEquals(this.date, this.day.getDate());
+    }
+
+    @Test
+    public void testSetDate() {
+        LocalDate newDate = LocalDate.of(2020, 1, 2);
+        assertDoesNotThrow(() -> this.day.setDate(newDate));
+        assertEquals(newDate, this.day.getDate());
+    }
+
+}
