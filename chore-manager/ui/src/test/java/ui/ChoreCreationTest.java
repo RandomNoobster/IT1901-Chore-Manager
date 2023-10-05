ec7ff4286e48bdd3c218cb2cdd2babc93ec84928
Lasse Bærland Strand <lassebs@stud.ntnu.no>
2023-10-05 15:55:51 +0200

    Added chore creation test

---
+++ chore-manager/ui/src/test/java/ui/ChoreCreationTest.java
@@ -0,0 +1,93 @@
+package ui;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.matcher.control.LabeledMatchers;
+import org.testfx.util.WaitForAsyncUtils;
+
+import core.Data.Chore;
+import core.Data.Person;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import persistence.FileHandling.Storage;
+
+public class ChoreCreationTest extends ApplicationTest {
+
+    private Parent root;
+    private ChoreCreationController controller;
+    private final String filePath = "chore-manager-data-ui-test.json";
+    private final Person testPerson = new Person("Test");
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("ChoreCreation.fxml"));
+        this.root = fxmlLoader.load();
+        this.controller = fxmlLoader.getController();
+
+        // CSS
+        Scene scene = new Scene(this.root);
+        scene.getStylesheets().add(this.getClass().getResource("Style.css").toExternalForm());
+
+        // Title
+        stage.setTitle("Chore Manager");
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @BeforeEach
+    public void setupItems() {
+        Storage.deleteInstance();
+        Storage.getInstance(this.filePath);
+        Storage.getInstance().addPerson(this.testPerson);
+    }
+
+    @AfterEach
+    public void clearItems() {
+        Storage.getInstance().deleteFile();
+    }
+
+    private void click(String... labels) {
+        for (var label : labels) {
+            this.clickOn(LabeledMatchers.hasText(label));
+        }
+    }
+
+    @Test
+    public void testCreateChore() {
+        List<Chore> savedChores = Storage.getInstance().getChoresList();
+        System.out.println(savedChores.size());
+        WaitForAsyncUtils.waitForFxEvents();
+
+        TextField name = this.lookup("#name").query();
+        this.interact(() -> {
+            name.setText("Bob");
+        });
+
+        ComboBox<String> comboBox = this.lookup("#personsMenu").query();
+        this.interact(() -> {
+            comboBox.getSelectionModel().select(0);
+        });
+
+        this.click("Create");
+
+        try {
+            Thread.sleep(15000);
+        } catch (InterruptedException e) {
+        }
+
+        WaitForAsyncUtils.waitForFxEvents();
+        assertTrue(savedChores.size() + 1 == Storage.getInstance().getChoresList().size());
+    }
+}
