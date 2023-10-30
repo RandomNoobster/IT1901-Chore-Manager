603ccb3ac94e521aa883a7cea2ef05b8c4c0ea74
Lasse Bærland Strand <lassebs@stud.ntnu.no>
2023-10-30 13:48:48 +0100

    Rewrote how EnvironmentConfigurator finds the env files

--- chore-manager/persistence/src/main/java/persistence/fileHandling/EnvironmentConfigurator.java
+++ chore-manager/persistence/src/main/java/persistence/fileHandling/EnvironmentConfigurator.java
@@ -1,5 +1,6 @@
 package persistence.fileHandling;
 
+import java.io.File;
 import java.util.Properties;
 
 import io.github.cdimascio.dotenv.Dotenv;
@@ -9,6 +10,7 @@ import io.github.cdimascio.dotenv.Dotenv;
  */
 public class EnvironmentConfigurator {
 
+    private static final String ROOT_MODULE_DIRECTORY = "chore-manager";
     private Properties properties = new Properties();
 
     /**
@@ -25,6 +27,30 @@ public class EnvironmentConfigurator {
     }
 
     /**
+     * Returns the absolute path of the root module directory. Assumes the name of the root module
+     * directory is `chore-manager` and by definition the root must be above the
+     * searchFromDirectory. Recursively searches for the root module directory.
+     * 
+     * @param searchFromDirectory The directory to start searching from
+     * @return The absolute path of the root module directory
+     */
+    private static String findRootModuleDirectory(String searchFromDirectory) {
+        File currentDir = new File(searchFromDirectory);
+
+        if (currentDir.getName().equals(ROOT_MODULE_DIRECTORY)) {
+            return currentDir.getAbsolutePath();
+        }
+
+        String parentDirectory = currentDir.getParent();
+        if (parentDirectory != null) {
+            return findRootModuleDirectory(parentDirectory);
+        }
+
+        // If no root module directory is found, throw an exception
+        throw new RuntimeException("Could not find root module directory");
+    }
+
+    /**
      * This constructor reads the current environment and gets the corresponding .env file. Then
      * loads it into properties. If no environment is set, it default to 'development'
      */
@@ -32,8 +58,10 @@ public class EnvironmentConfigurator {
         String environment = System.getProperty("env", "development"); // Defaults to
                                                                        // "development"
         String envFileName = ".env." + environment;
+        String rootModuleDirectory = findRootModuleDirectory(System.getProperty("user.dir"));
 
-        Dotenv dotenv = Dotenv.configure().directory("../../.").filename(envFileName).load();
+        Dotenv dotenv = Dotenv.configure().directory(rootModuleDirectory).filename(envFileName)
+                .load();
         String saveFilePath = dotenv.get("SAVE_FILE_PATH");
         String saveFilePathAlt = dotenv.get("SAVE_FILE_PATH_ALT", null);
 
