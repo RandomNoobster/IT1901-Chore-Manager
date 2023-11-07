5ebaa7d77972a2f9aefc37bfd78a100ad93aaa7a
Lasse Bærland Strand <lassebs@stud.ntnu.no>
2023-11-07 19:58:49 +0100

    Connect LeaderboardController to API

--- chore-manager/ui/src/main/java/ui/Leaderboard.java
+++ chore-manager/ui/src/main/java/ui/Leaderboard.java
@@ -2,43 +2,54 @@ package ui;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import core.State;
 import core.data.Chore;
-import core.data.Person;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.util.Pair;
+import ui.dataAccessLayer.DataAccess;
 
+/**
+ * The Leaderboard class handles displaying the leaderboard in the UI.
+ */
 public class Leaderboard {
 
+    private DataAccess dataAccess;
+
     @FXML
-    private ListView leaderboard;
+    private ListView<String> leaderboard;
 
     @FXML
     public void initialize() {
+        this.dataAccess = App.getDataAccess();
         this.displayLeaderboard();
     }
 
+    /**
+     * Displays the leaderboard in the UI.
+     */
     public void displayLeaderboard() {
 
         this.leaderboard.getItems().clear();
         List<Pair<String, Integer>> pointsList = new ArrayList<>();
 
-        for (Person p : State.getInstance().getCurrentCollective().getPersonsList()) {
-            int points = 0;
-            for (Chore c : p.getChores()) {
-                if (c.getChecked()) {
-                    points += c.getPoints();
-                }
+        List<Chore> chores = this.dataAccess.getChores();
+        HashMap<String, Integer> points = new HashMap<String, Integer>();
+        for (Chore chore : chores) {
+            if (chore.getChecked()) {
+                Integer prevPoints = points.getOrDefault(chore.getAssignedTo(), 0);
+                points.put(chore.getAssignedTo(), prevPoints + chore.getPoints());
             }
-            pointsList.add(new Pair(p.getDisplayName(), points));
         }
 
+        pointsList = points.entrySet().stream()
+                .map(e -> new Pair<String, Integer>(e.getKey(), e.getValue()))
+                .collect(Collectors.toList());
         Collections.sort(pointsList, (p1, p2) -> Integer.compare(p2.getValue(), p1.getValue()));
 
         ObservableList<String> items = FXCollections.observableArrayList(pointsList.stream()
