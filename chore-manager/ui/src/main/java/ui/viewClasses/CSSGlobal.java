b9fb4b58c0e1f5c9c539d71d0461951d6ed040ea
Sebastian Munthe <sebasmu@stud.ntnu.no>
2023-11-13 14:54:56 +0100

    Changed value from 10 to 12

--- chore-manager/ui/src/main/java/ui/viewClasses/CSSGlobal.java
+++ chore-manager/ui/src/main/java/ui/viewClasses/CSSGlobal.java
@@ -1,7 +1,7 @@
 package ui.viewClasses;
 
 public class CSSGlobal {
-    public static final int SCROLLBAR_WIDTH = 10;
+    public static final int SCROLLBAR_WIDTH = 12;
     public static final int BUTTON_HEIGHT = 30;
     public static final int ACCOUNT_FOR_PADDING = 15;
     public static final int TOP_LABEL_HEIGHT = 50;
