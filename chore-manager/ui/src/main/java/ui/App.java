fe30d7aee3c694b13313f749e5b81aa6626713df
Lasse Bærland Strand <lassebs@stud.ntnu.no>
2023-11-15 09:47:40 +0100

    Fix environments when env file is not found in JPackage

--- chore-manager/ui/src/main/java/ui/App.java
+++ chore-manager/ui/src/main/java/ui/App.java
@@ -123,8 +123,15 @@ public class App extends Application {
      * Gets the data access layer.
      */
     public static DataAccess getDataAccess() {
-        EnvironmentConfigurator configurator = new EnvironmentConfigurator();
-        URI apiBaseEndpoint = configurator.getAPIBaseEndpoint();
+        URI apiBaseEndpoint;
+        try {
+            EnvironmentConfigurator configurator = new EnvironmentConfigurator();
+            apiBaseEndpoint = configurator.getAPIBaseEndpoint();
+        } catch (RuntimeException e) {
+            System.out.println("COULD NOT FIND ENV FILES. USING LOCALHOST FOR THE API.");
+            apiBaseEndpoint = URI.create("http://localhost:8080/");
+        }
+
         if (apiBaseEndpoint != null) {
             DataAccess dataAccess = new RemoteDataAccess(apiBaseEndpoint);
             setCorrectMode(dataAccess);
